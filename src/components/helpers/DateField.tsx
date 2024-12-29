d57063f7c712ffeb571d818a5914a67d17b5421c
freddy24-7 <freddy.austli@aol.com>
2024-12-30 00:15:14 +0100

    debugging date functionality

--- src/components/helpers/DateField.tsx
+++ src/components/helpers/DateField.tsx
@@ -18,9 +18,9 @@ const DateField = ({ errors, onChange }: DateProps): React.ReactElement => {
   const today = new Date();
 
   const dateTimeRegex =
-    /^([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9](:([0-5][0-9]|60))?(\.[0-9]{1,9})?)?)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)?)?)?$/;
+    /^([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9](:([0-5][0-9]|60))?(\.[0-9]{1,9})?)?)?([Z+\-]((0[0-9]|1[0-3]):[0-5][0-9]|14:00)?)?)?$/;
 
-  function handleParseDateString(dateString) {
+  function handleParseDateString(dateString: string): Date {
     if (dateTimeRegex.test(dateString)) {
       return toDate(dateString);
     }
