cfe7486e28d55c7788e6cda3abe853133bdace41
freddy24-7 <freddy.austli@aol.com>
2024-12-30 11:06:24 +0100

    debugging date functionality

--- src/lib/services/createTrainingService.ts
+++ src/lib/services/createTrainingService.ts
@@ -1,5 +1,5 @@
 import { Training } from '@prisma/client';
-import { parseISO, endOfDay } from 'date-fns';
+import { parseISO, endOfDay, addHours } from 'date-fns';
 
 import prisma from '@/lib/prisma';
 import { PlayerAtTraining } from '@/types/user-types';
@@ -8,7 +8,14 @@ export async function createTraining(
   date: string,
   players: PlayerAtTraining[]
 ): Promise<Training> {
-  const chosenDate = endOfDay(parseISO(date));
+  const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  console.log('User timezone:', userTimezone);
+
+  let chosenDate = endOfDay(parseISO(date));
+
+  if (userTimezone === 'UTC') {
+    chosenDate = addHours(chosenDate, 12);
+  }
 
   return prisma.training.create({
     data: {
