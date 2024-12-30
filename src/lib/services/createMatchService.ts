ab7950e56a74c46a8493890caf53e4787afb1d8a
freddy24-7 <freddy.austli@aol.com>
2024-12-30 14:41:58 +0100

    debugging date functionality

--- src/lib/services/createMatchService.ts
+++ src/lib/services/createMatchService.ts
@@ -1,5 +1,4 @@
 import { PouleOpponents, Match } from '@prisma/client';
-import { parseISO, endOfDay, addHours } from 'date-fns';
 
 import prisma from '@/lib/prisma';
 
@@ -15,21 +14,10 @@ export async function createMatch(
   pouleOpponentId: number,
   date: string
 ): Promise<Match> {
-  //This code section below is written to also work in production on vercel, which is given in UTC timezone.
-  //As the user in production is in UTC timezone , we need to add hours to the date to make it work in CET.
-  const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  console.log('User timezone:', userTimezone);
-
-  let matchDate = endOfDay(parseISO(date));
-
-  if (userTimezone === 'UTC') {
-    matchDate = addHours(matchDate, 0);
-  }
-
   return prisma.match.create({
     data: {
       pouleOpponentId,
-      date: matchDate,
+      date: new Date(date),
       createdAt: new Date(),
     },
   });
