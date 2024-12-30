62a3b6b7ce0ec1102728da3ec8ba8771b97d7ed6
freddy24-7 <freddy.austli@aol.com>
2024-12-30 10:32:49 +0100

    debugging date functionality

--- src/lib/services/createTrainingService.ts
+++ src/lib/services/createTrainingService.ts
@@ -1,4 +1,5 @@
 import { Training } from '@prisma/client';
+import { parseISO, startOfDay } from 'date-fns';
 
 import prisma from '@/lib/prisma';
 import { PlayerAtTraining } from '@/types/user-types';
@@ -7,17 +8,11 @@ export async function createTraining(
   date: string,
   players: PlayerAtTraining[]
 ): Promise<Training> {
-  const chosenDate = new Date(date);
-
-  const year = chosenDate.getFullYear();
-  const month = chosenDate.getMonth();
-  const day = chosenDate.getDate();
-
-  const forcedUTC = new Date(Date.UTC(year, month, day, 0, 0, 0));
+  const chosenDate = startOfDay(parseISO(date));
 
   return prisma.training.create({
     data: {
-      date: forcedUTC,
+      date: chosenDate,
       createdAt: new Date(),
       trainingPlayers: {
         create: players.map((player) => ({
