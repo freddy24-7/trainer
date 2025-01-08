4eae58557c684f41e52c7524e63d46dcd7120e67
freddy24-7 <freddy.austli@aol.com>
2025-01-08 09:08:20 +0100

    repairing responsive design

--- src/components/navigation/ThemeToggleButton.tsx
+++ src/components/navigation/ThemeToggleButton.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@nextui-org/react';
 import { useTheme } from 'next-themes';
 import React from 'react';
 import { FiSun, FiMoon } from 'react-icons/fi';
@@ -9,13 +8,12 @@ export function ThemeToggleButton(): React.ReactElement {
   const { theme, setTheme } = useTheme();
 
   return (
-    <Button
-      variant="bordered"
-      className="ml-4 text-white"
+    <button
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
       aria-label={toggleThemeAriaLabel}
+      className="ml-4 border border-white rounded text-white px-4 py-2 flex items-center"
     >
       {theme === 'light' ? <FiMoon size={20} /> : <FiSun size={20} />}
-    </Button>
+    </button>
   );
 }
